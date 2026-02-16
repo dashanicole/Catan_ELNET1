3d23dc8d89d62b3ca252f312802b8e6f0c9066a9
dashanicole <diethercatangwapz@gmail.com>
2026-02-16 10:26:13 +0800

    ELNET1

---
+++ SampleApp/SampleApp/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using CATAN_LabActivity1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CATAN_LabActivity1.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //Student student = new Student()
+            //{
+            //    Id = 123456789,
+            //    Name = "Diether D. Catan",
+            //    Course = "BSCS"
+            //};
+
+            //return View(student);
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Student pupil)
+        {
+            return View(pupil);
+        }
+    }
+}
